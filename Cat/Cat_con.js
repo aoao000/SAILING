73d616e645848a78bed78b01a5f4da1150623dd0
aoao000 <80384188+aoao000@users.noreply.github.com>
2022-12-23 23:49:48 +0800

    Add files via upload

---
+++ Cat/Cat_con.js
@@ -0,0 +1,26 @@
+let bg_navbar = document.getElementById('top_bar');
+let a_top = document.getElementById('to_top');
+
+function show() {
+    if(window.scrollY > 1000 ){
+        a_top.style.opacity = '1';
+        a_top.style.right = '50px';
+    }
+    if(window.scrollY < 1000 ){
+        a_top.style.opacity = '0';
+        a_top.style.right = '-100px';
+
+    }
+    if(window.scrollY > 200){
+        bg_navbar.style.background = "rgba(0,0,0,0.5)";
+    }
+    if(window.scrollY < 200){
+        bg_navbar.style.background = "none";
+    }
+}
+function to_top() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
