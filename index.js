7966b85e9fa05f291cc874d9a0ab83901d4cd312
aoao000 <80384188+aoao000@users.noreply.github.com>
2022-12-25 13:34:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function () {
+    let counter = 0;
+    setInterval(() => {
+        if (counter == 2) {
+            clearInterval();
+            document.getElementById("loading_ing").style.opacity = "0";
+            document.getElementById("loading_ing").style.zIndex = "-10000";
+            document.getElementById("scroll").style.overflow = "scroll";
+        }
+        else {
+            counter += 1;
+        }
+    }, 1000)
+})
+
+let bg_navbar = document.getElementById('top_bar');
+let a_top = document.getElementById('to_top');
+let leftFunction = document.getElementById('leftFunction');
+
+function show() {
+    if(window.scrollY > 1000 ){
+        a_top.style.opacity = '1';
+        a_top.style.right = '50px';
+        
+    }
+    if(window.scrollY < 1000 ){
+        a_top.style.opacity = '0';
+        a_top.style.right = '-100px';
+    }
+    if(window.scrollY > 200){
+        bg_navbar.style.background = "rgba(0,0,0,0.5)";
+    }
+    if(window.scrollY < 200){
+        bg_navbar.style.background = "none";
+    }
+    if(window.scrollY > 500){
+        leftFunction.style.opacity = '1';
+    }
+    if(window.scrollY < 500){
+        leftFunction.style.opacity = '0';
+    }
+}
+function to_top() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
